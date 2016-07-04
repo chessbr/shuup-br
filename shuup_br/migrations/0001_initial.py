244e6d0bd38cd348b3e74ca13d3b32cd60335fe3
Christian Hess <christianhess.rlz@gmail.com>
2016-07-04 11:18:50 -0300

    Fixed migration issues

--- shuup_br/migrations/0001_initial.py
+++ shuup_br/migrations/0001_initial.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import shuup_br.models
-import enumfields.fields
-from django.conf import settings
 import django.utils.timezone
+from django.conf import settings
+import enumfields.fields
 import shuup.core.models
 
 
@@ -19,17 +19,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ShuupBRUser',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('password', models.CharField(verbose_name='password', max_length=128)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
-                ('is_superuser', models.BooleanField(verbose_name='superuser status', default=False, help_text='Designates that this user has all permissions without explicitly assigning them.')),
-                ('email', models.EmailField(verbose_name='email address', error_messages={'unique': 'A user with that email already exists.'}, max_length=254, unique=True)),
-                ('is_staff', models.BooleanField(verbose_name='staff status', default=False, help_text='Designates whether the user can log into this admin site.')),
-                ('is_active', models.BooleanField(verbose_name='active', default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
-                ('date_joined', models.DateTimeField(verbose_name='date joined', default=django.utils.timezone.now)),
-                ('person_type', enumfields.fields.EnumField(verbose_name='Tipo de pessoa', default='PF', max_length=2, enum=shuup_br.models.PersonType)),
-                ('groups', models.ManyToManyField(verbose_name='groups', to='auth.Group', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', blank=True, related_query_name='user')),
-                ('user_permissions', models.ManyToManyField(verbose_name='user permissions', to='auth.Permission', help_text='Specific permissions for this user.', related_name='user_set', blank=True, related_query_name='user')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('email', models.EmailField(unique=True, max_length=254, verbose_name='email address', error_messages={'unique': 'A user with that email already exists.'})),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('person_type', enumfields.fields.EnumField(default='PF', max_length=2, verbose_name='Tipo de pessoa', enum=shuup_br.models.PersonType)),
+                ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
             options={
                 'verbose_name': 'user',
@@ -42,60 +42,60 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CompanyInfo',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('name', models.CharField(verbose_name='Razão social', max_length=80)),
-                ('cnpj', models.CharField(verbose_name='CNPJ', max_length=18, validators=[shuup_br.models.validate_cnpj])),
-                ('ie', models.CharField(null=True, verbose_name='Inscrição estadual', blank=True, max_length=30)),
-                ('im', models.CharField(null=True, verbose_name='Inscrição municipal', blank=True, max_length=30)),
-                ('taxation', enumfields.fields.EnumField(verbose_name='Tipo de tributação', default='e', max_length=1, enum=shuup_br.models.Taxation)),
-                ('responsible', models.CharField(verbose_name='Nome do responsável', max_length=60)),
-                ('user', models.OneToOneField(verbose_name='pessoa juridica', to=settings.AUTH_USER_MODEL, related_name='pj_person')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=80, verbose_name='Raz\xe3o social')),
+                ('cnpj', models.CharField(max_length=18, verbose_name='CNPJ', validators=[shuup_br.models.validate_cnpj])),
+                ('ie', models.CharField(max_length=30, null=True, verbose_name='Inscri\xe7\xe3o estadual', blank=True)),
+                ('im', models.CharField(max_length=30, null=True, verbose_name='Inscri\xe7\xe3o municipal', blank=True)),
+                ('taxation', enumfields.fields.EnumField(default='e', max_length=1, verbose_name='Tipo de tributa\xe7\xe3o', enum=shuup_br.models.Taxation)),
+                ('responsible', models.CharField(max_length=60, verbose_name='Nome do respons\xe1vel')),
+                ('user', models.OneToOneField(related_name='pj_person', verbose_name='pessoa juridica', to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'verbose_name': 'Pessoa jurídica',
-                'verbose_name_plural': 'Pessoas jurídicas',
+                'verbose_name': 'Pessoa jur\xeddica',
+                'verbose_name_plural': 'Pessoas jur\xeddicas',
             },
         ),
         migrations.CreateModel(
             name='ExtraImmutableAddress',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('numero', models.CharField(verbose_name='Número', max_length=20)),
-                ('cel', models.CharField(null=True, verbose_name='Telefone celular', blank=True, max_length=40)),
-                ('ponto_ref', models.CharField(null=True, verbose_name='Ponto de referência', blank=True, max_length=60)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('numero', models.CharField(max_length=20, verbose_name='N\xfamero')),
+                ('cel', models.CharField(max_length=40, null=True, verbose_name='Telefone celular', blank=True)),
+                ('ponto_ref', models.CharField(max_length=60, null=True, verbose_name='Ponto de refer\xeancia', blank=True)),
             ],
             options={
-                'verbose_name': 'Endereço imutável - Informação extra',
-                'verbose_name_plural': 'Endereços imutáveis - Informação extra',
+                'verbose_name': 'Endere\xe7o imut\xe1vel - Informa\xe7\xe3o extra',
+                'verbose_name_plural': 'Endere\xe7os imut\xe1veis - Informa\xe7\xe3o extra',
             },
         ),
         migrations.CreateModel(
             name='ExtraMutableAddress',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('numero', models.CharField(verbose_name='Número', max_length=20)),
-                ('cel', models.CharField(null=True, verbose_name='Telefone celular', blank=True, max_length=40)),
-                ('ponto_ref', models.CharField(null=True, verbose_name='Ponto de referência', blank=True, max_length=60)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('numero', models.CharField(max_length=20, verbose_name='N\xfamero')),
+                ('cel', models.CharField(max_length=40, null=True, verbose_name='Telefone celular', blank=True)),
+                ('ponto_ref', models.CharField(max_length=60, null=True, verbose_name='Ponto de refer\xeancia', blank=True)),
             ],
             options={
-                'verbose_name': 'Endereço mutável - Informação extra',
-                'verbose_name_plural': 'Endereços mutáveis - Informação extra',
+                'verbose_name': 'Endere\xe7o mut\xe1vel - Informa\xe7\xe3o extra',
+                'verbose_name_plural': 'Endere\xe7os mut\xe1veis - Informa\xe7\xe3o extra',
             },
         ),
         migrations.CreateModel(
             name='PersonInfo',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('name', models.CharField(verbose_name='Nome completo', max_length=60)),
-                ('cpf', models.CharField(verbose_name='CPF', max_length=14, validators=[shuup_br.models.validate_cpf])),
-                ('rg', models.CharField(verbose_name='Identidade', max_length=30)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=60, verbose_name='Nome completo')),
+                ('cpf', models.CharField(max_length=14, verbose_name='CPF', validators=[shuup_br.models.validate_cpf])),
+                ('rg', models.CharField(max_length=30, verbose_name='Identidade')),
                 ('birth_date', models.DateField(verbose_name='Data de nascimento')),
-                ('gender', enumfields.fields.EnumField(verbose_name='Sexo', default='u', max_length=4, enum=shuup.core.models.Gender)),
-                ('user', models.OneToOneField(verbose_name='pessoa_fisica', to=settings.AUTH_USER_MODEL, related_name='pf_person')),
+                ('gender', enumfields.fields.EnumField(default='u', max_length=4, verbose_name='Sexo', enum=shuup.core.models.Gender)),
+                ('user', models.OneToOneField(related_name='pf_person', verbose_name='pessoa_fisica', to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'verbose_name': 'Pessoa física',
-                'verbose_name_plural': 'Pessoas físicas',
+                'verbose_name': 'Pessoa f\xedsica',
+                'verbose_name_plural': 'Pessoas f\xedsicas',
             },
         ),
     ]
--- shuup_br/migrations/0002_auto_20160704_1417.py
+++ shuup_br/migrations/0002_auto_20160704_1417.py
@@ -7,6 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('shuup', '0001_initial'),
         ('shuup_br', '0001_initial'),
     ]
 
